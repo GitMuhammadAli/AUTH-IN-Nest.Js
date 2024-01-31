3f035d2fe99fd7c2b9a53ddd1e318b94fca2fd39
GitMuhammadAli <malingxoft@gmail.com>
2024-02-01 01:29:12 +0500

    commit

--- src/dto/delete-user.dto/delete-user.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DeleteUserDtoFromDtoFolder } from './delete-user.dto';
-
-describe('DeleteUserDto', () => {
-  it('should be defined', () => {
-    expect(new DeleteUserDtoFromDtoFolder()).toBeDefined();
-  });
-});
