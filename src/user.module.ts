54527c7b44e41e186c5c99b2e3898063f100bd8b
GitMuhammadAli <malingxoft@gmail.com>
2024-02-01 23:29:19 +0500

    cmmit

--- src/user.module.ts
+++ src/user.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth/auth.service';
 import { Userschema, Myuserschema } from './models/user.schema';
+import { Loginschema } from './models/login.schema';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 
 @Module({
     imports: [
-        AuthModule,
+        Loginschema,
         MongooseModule.forFeature([{ name: Userschema.name, schema: Myuserschema }]),
     ],
     controllers: [AppController],
